e602492d701999e64c883595b4a9cd9ad0a1ebc1
Ari2004coder <roy2004arijit@gmail.com>
2024-09-21 17:56:57 +0000

    container config done

---
+++ .devcontainer/01_basics/VariableAndConstant.js
@@ -0,0 +1,12 @@
+const accountId=14435;//can't change anymore; ";"optional
+let accountEmail="abc@gmail.com";
+var accountPassward="1234";//not preferable
+name="arijit";//not prefferable
+// accountId=123; not allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassward,name])
+ accountEmail="a.gmail.com"
+ accountPassward="121"
+ console.table([accountId, accountEmail, accountPassward,name])
+
+
